2cae18b5d1d867a1f92535a97806395e5d325e32
lukeisuh <luke@issilly.fr>
2024-02-12 06:52:09 +1100

    i added a button today :)

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -10,9 +10,11 @@
   </div>
 
   <h1 class="mt-2 text-6xl text-white-900"><b> Hi! </b> </h1>
-  <p class="mt-5 text-6xl text-white"> my name is <b> Luke </b> </p>
-  <p class="text-5xl text-white"> I am <b> 17 </b> years old </p>
-  <p class="text-5xl text-white"> I am <b> Australian </b> too </p>
+  <p class="mt-5 text-6xl text-white"> My name is <b> Luke </b> </p>
+  <p class="text-4xl text-white"> I am a <b> 17 </b> year old <b> Australian </b> <br>
+    whos hoping to get more into web development</p>
+  <p class="-mt-2 text-1xl text-white"> <i> and maybe <b> cyber security </b> because that pays a lot <br> </i> </p>
+  <p class="-mt-2 text-1xl text-white"> <i> or maybe <b> pc building </b> because thats fun </i> </p>
   <p class="mt-5 text-6xl text-white"> and I love doing really <b> stupid shit </b> </p>
   <p class="-mt-1 text-6xl text-white"> <i> like: </i> </p>
 
@@ -24,13 +26,19 @@
 
   <div class="mt-5 flex gap-4 justify-center">
     <a href="https://catboys.site" target="_blank" class="CATBG bg-cover text-white font-bold py-5 px-8 rounded-lg text-shadow-black border border-white">
-      a site for random cat pictures!
+      Making a site for random cat pictures!
     </a>
   </div>
 
   <div class="mt-5 flex gap-4 justify-center">
     <a href="https://ai.luke.tf" target="_blank" class="AIBG bg-cover text-white font-bold py-5 px-8 rounded-lg text-shadow-black border border-white">
-      a site to show off AI website making capabilities!
+      Making a site to show off AI website making capabilities!
+    </a>
+  </div>
+
+  <div class="mt-5 flex gap-4 justify-center">
+    <a href="https://ai.luke.tf" target="_blank" class="PPBG bg-cover text-white font-bold py-5 px-8 rounded-lg text-shadow-black border border-white">
+      Making a REALLY bad Pepsi mod for Minecraft!
     </a>
   </div>
 
